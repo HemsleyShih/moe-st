0f2194062e5f642e1f4cec72069e395b90e921f6
yiling.liu <yiling.liu@PCC-3000005332.pcc.com.tw>
2018-08-08 14:06:40 +0800

    yiling upload

---
+++ src/test/java/pcc/pouchen/com/AppTest/yilingTEST.java
@@ -0,0 +1,10 @@
+package pcc.pouchen.com.AppTest;
+
+public class yilingTEST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
